c4d27c458d8ac125b18ce75fee0838cf80d3e363
Mitxel <mitxel@tresipunt.com>
2018-03-05 17:45:53 +0100

    set version.php

--- version.php
+++ version.php
@@ -29,8 +29,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_tictactoe';
 $plugin->version = 2018030500;
-$plugin->release = 'v0.0';
-$plugin->requires = 2017051500;
+$plugin->release = 'v0.1';
+$plugin->requires = 2017111301; // Requires Moodle 3.4
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
 $plugin->dependencies = array();
